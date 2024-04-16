63adbe7d58615f39232649738230d7266d0c319c
Aurelius1688 <137530953+Aurelius1688@users.noreply.github.com>
2024-04-16 12:17:50 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -422,30 +422,6 @@ cover: 'https://aws.ezdiy.top/usr/uploads/music.jpg'
 cover: 'https://aws.ezdiy.top/usr/uploads/music.jpg'
 
 }
-
-const colorThief = new ColorThief();
-const image = new Image();
-const xhr = new XMLHttpRequest();
-const setTheme = (index) => {
-    if (!ap.list.audios[index].theme) {
-        xhr.onload = function(){
-            let coverUrl = URL.createObjectURL(this.response);
-            image.onload = function(){
-                let color = colorThief.getColor(image);
-                ap.theme(`rgb(${color[0]}, ${color[1]}, ${color[2]})`, index);
-                URL.revokeObjectURL(coverUrl)
-            };
-            image.src = coverUrl;
-        }
-        xhr.open('GET', ap.list.audios[index].cover, true);
-        xhr.responseType = 'blob';
-        xhr.send();
-    }
-};
-setTheme(ap.list.index);
-ap.on('listswitch', (index) => {
-    setTheme(index);
-});
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
